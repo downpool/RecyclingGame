5b4e92262aa2df2c3852c7f24f155b549128f5b2
downpool <downpool.choi@gmail.com>
2023-04-15 08:22:22 +0900

    moving effect apply

--- BookStack.swiftpm/ContentView.swift
+++ BookStack.swiftpm/ContentView.swift
@@ -3,7 +3,8 @@ import SwiftUI
 struct ContentView: View {
     @State var bookCount: Int = 0
     @State var isDrop: Bool = false
-//    @State var timer: Timer
+    @State var isMoving: Bool = false
+    //    @State var timer: Timer
     
     var body: some View {
         GeometryReader { geo in
@@ -39,6 +40,26 @@ struct ContentView: View {
                 
                 // MARK: block create here!
                 
+                //                Rectangle()
+                //                    .frame(width: 10, height: 10)
+                //                    .onAppear {
+                //                        withAnimation(self.moveTrash, <#() throws -> Void#>)
+                //                    }
+                
+                Button {
+                    
+                } label: {
+                    Image("can")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .offset(x: isMoving ? -150 : 150, y: -200)
+                        .animation(moveTrash)
+                        .onAppear {
+                            self.isMoving = true
+                        }
+                }
+                
+                
                 
                 HStack(spacing: 0) {
                     Image("woodbox")
@@ -63,7 +84,7 @@ struct ContentView: View {
                     .font(.system(size: 35))
                     .position(x: geo.size.width / 6, y: 700)
                 
-                Text("general")
+                Text("paper")
                     .bold()
                     .font(.system(size: 30))
                     .position(x: geo.size.width / 2, y: 700)
@@ -77,33 +98,11 @@ struct ContentView: View {
     }
 }
 
-func createTrash() -> Trash {
-    
-    let randomNumber: Int = Int.random(in: 0...2)
-    let randomTrash: Trash
-    switch (randomNumber) {
-        case 0:
-            randomTrash = Trash(type: .Can)
-            break
-            
-        case 1:
-            randomTrash = Trash(type: .Bottle)
-            break
-        
-        case 2:
-            randomTrash = Trash(type: .General)
-            break
-        
-        default:
-            randomTrash = Trash(type: .General)
-            break
-    }
-    
-    return randomTrash
-}
 
-func moveTrash(Trash: Trash) {
-    
+
+var moveTrash: Animation {
+    Animation.linear(duration: 2)
+        .repeatForever()
 }
 
 func dropTrash(Trash: Trash) {
@@ -118,17 +117,17 @@ func continueDrop(){
 
 func runEngine() {
     /*
-    while(!gameLose) {
-        moveTrash()
-        if(onTapGesture) {
-            dropTrash()
-            
-            stackCalculate()
-        }
+     while(!gameLose()) {
+     moveTrash()
+     if(onTapGesture) {
+     dropTrash()
      
-        continueDrop()
-    }
-    */
+     stackCalculate()
+     }
+     
+     continueDrop()
+     }
+     */
 }
 
 func gameLose() -> Bool {
@@ -140,44 +139,56 @@ func stackCalculate() {
     
 }
 
-func checkingGravity() {
-    
-}
-
-func collapsBook() {
+func checkRightTrash() {
     
 }
 
 struct Trash {
     let type: TrashType
-    var mainPoint: Double
-    let width: CGFloat
-    let height: CGFloat
     let image: Image
     
     init(type: TrashType) {
         self.type = type
         switch type {
             case .Can:
-                width = 120
-                mainPoint = 60
-                height = 40
-            case .General:
-                width = 100
-                mainPoint = 50
-                height = 25
+                image = Image("can")
+            case .Paper:
+                image = Image("paper")
             case .Bottle:
-                width = 75
-                mainPoint = 37.5
-                height = 20
+                image = Image("bottle")
         }
+    }
+    
+    func createTrash() -> Trash {
+        
+        let randomNumber: Int = Int.random(in: 0...2)
+        let randomTrash: Trash
+        switch (randomNumber) {
+            case 0:
+                randomTrash = Trash(type: .Can)
+                break
+                
+            case 1:
+                randomTrash = Trash(type: .Bottle)
+                break
+                
+            case 2:
+                randomTrash = Trash(type: .Paper)
+                break
+                
+            default:
+                randomTrash = Trash(type: .Paper)
+                break
+        }
+        
+        return randomTrash
     }
 }
 
 
 // MARK: books top pos is y: 110
 enum TrashType: CaseIterable {
-    case Can, General, Bottle
+    case Can, Paper, Bottle
 }
 
 
